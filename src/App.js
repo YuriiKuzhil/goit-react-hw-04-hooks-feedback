ab436423601a05b944bc655a24bb1457a31ea468
Yurii <yuriikuzhil@gmail.com>
2022-01-15 22:36:19 +0200

    Reworked file App with using HOOKS

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import FeedbackSection from './feedbackSection';
 import FeedbackOptions from './feedbackOptions';
 import Statistics from './statistics';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGoodFeedback] = useState(0);
+  const [neutral, setNeutralFeedback] = useState(0);
+  const [bad, setBadFeedback] = useState(0);
+
+  const onLeaveFeedback = feedback => {
+    switch (feedback) {
+      case 'good':
+        setGoodFeedback(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutralFeedback(state => state + 1);
+        break;
+      case 'bad':
+        setBadFeedback(state => state + 1);
+        break;
+
+      default:
+        return;
+    }
   };
-  onLeaveFeedback = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+  const positiveFeedbackPercentage = () => {
+    const totalFeedbacks = countTotalFeedback();
+    return Math.floor((good / totalFeedbacks) * 100);
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const goodFeedbacks = this.state.good;
-    const totalFeedbacks = this.countTotalFeedback();
-    return Math.floor((goodFeedbacks / totalFeedbacks) * 100);
-  };
-
-  render() {
-    const totalFeedbacks = this.countTotalFeedback();
-    const positiveFeedbacks = this.countPositiveFeedbackPercentage();
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
-
-    return (
-      <FeedbackSection title="Please leave feedback">
-        <FeedbackOptions
-          options={options}
-          onLeaveFeedback={this.onLeaveFeedback}
-        />
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={totalFeedbacks}
-          positivePercentage={positiveFeedbacks}
-        />
-      </FeedbackSection>
-    );
-  }
+  const kindOfFeedbacks = ['good', 'neutral', 'bad'];
+  return (
+    <FeedbackSection title="Please leave feedback">
+      <FeedbackOptions
+        options={kindOfFeedbacks}
+        onLeaveFeedback={onLeaveFeedback}
+      />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={countTotalFeedback()}
+        positivePercentage={positiveFeedbackPercentage()}
+      />
+    </FeedbackSection>
+  );
 }
-export default App;
